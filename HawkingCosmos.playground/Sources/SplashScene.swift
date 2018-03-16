261f6fa60666345b325fa60b7a775d42d207f360
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-15 22:12:57 -0700

    go to

--- HawkingCosmos.playground/Sources/SplashScene.swift
+++ HawkingCosmos.playground/Sources/SplashScene.swift
@@ -3,4 +3,12 @@ public class SplashSceneFile: SKScene {
     override public func sceneDidLoad() {
         super.sceneDidLoad()
     }
+    override public func keyUp(with event: NSEvent) {
+        goToScene()
+    }
+    func goToScene() {
+        let mainMenuScene: SKScene = MainMenuSceneFile(fileNamed: "scenes/MainMenuScene")!
+        mainMenuScene.scaleMode = .aspectFit
+        view?.presentScene(mainMenuScene, transition: SKTransition.fade(withDuration: 2.0))
+    }
 }
