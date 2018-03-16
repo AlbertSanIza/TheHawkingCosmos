df4b56b7d8dfee70777d60fb817b9debec5a06b1
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-15 21:47:46 -0700

    changed sizes

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -1,10 +1,8 @@
 import SpriteKit
 import PlaygroundSupport
 
-//let mainFrame = CGRect(x: 0, y: 0, width: 1200, height: 900)
-let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
-//let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
-//let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
+//let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
 let mainView = SKView(frame: mainFrame)
 
 mainView.showsFPS = true
