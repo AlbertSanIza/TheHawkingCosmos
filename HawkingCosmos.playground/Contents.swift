b2848008249e011f903fe17a347772d3154f63fd
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-15 22:49:06 -0700

    moved code to single file

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -1,16 +1,38 @@
+//----------------------------------------------------------------------------------------------------------------------------------
 import SpriteKit
 import PlaygroundSupport
-
+//----------------------------------------------------------------------------------------------------------------------------------
+public class SplashSceneFile: SKScene {
+    override public func sceneDidLoad() {
+        super.sceneDidLoad()
+    }
+    override public func keyUp(with event: NSEvent) {
+        goToScene()
+    }
+    func goToScene() {
+        let mainMenuScene: SKScene = MainMenuSceneFile(fileNamed: "scenes/MainMenuScene")!
+        mainMenuScene.scaleMode = .aspectFit
+        view?.presentScene(mainMenuScene, transition: SKTransition.fade(withDuration: 2.0))
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+public class MainMenuSceneFile: SKScene {
+    override public func sceneDidLoad() {
+        super.sceneDidLoad()
+    }
+    override public func keyUp(with event: NSEvent) {
+        
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------
 let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 //let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
 let mainView = SKView(frame: mainFrame)
-
 mainView.showsFPS = true
 mainView.showsNodeCount = true
 mainView.showsPhysics = false
-
 let splashScene: SKScene = SplashSceneFile(fileNamed: "scenes/SplashScene.sks")!
 splashScene.scaleMode = .aspectFit
 mainView.presentScene(splashScene)
-
 PlaygroundPage.current.liveView = mainView
+//----------------------------------------------------------------------------------------------------------------------------------
