396cbb17d5cc0dbf0169e3d4b72a941867afdeb1
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-15 18:30:01 -0700

    extra space

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -6,6 +6,7 @@ let mainFrame = CGRect(x: 0, y: 0, width: 1000, height: 750)
 //let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 let mainView = SKView(frame: mainFrame)
+
 mainView.showsFPS = true
 mainView.showsNodeCount = true
 mainView.showsPhysics = false
