4d123d2e63297540c1ad792f8becb1fd88ce7410
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-15 18:51:13 -0700

    deleted creator

--- HawkingCosmos.playground/Sources/SplashScene.swift
+++ HawkingCosmos.playground/Sources/SplashScene.swift
@@ -2,8 +2,5 @@ import SpriteKit
 public class SplashSceneFile: SKScene {
     override public func sceneDidLoad() {
         super.sceneDidLoad()
-        if let sksStars: SKSpriteNode = childNode(withName: "sksStars") as! SKSpriteNode? {
-            print(sksStars)
-        }
     }
 }
