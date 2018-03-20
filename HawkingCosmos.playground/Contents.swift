1711c5bfddc0803c66fa2e6e75277e2d362a5c7b
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-20 13:48:38 -0700

    comment old skscenees

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -141,12 +141,15 @@ public class sceneFiles {
 //let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
-let mainView = SKView(frame: mainFrame)
-mainView.showsFPS = true
-mainView.showsNodeCount = true
-mainView.showsPhysics = false
-let splashScene: SKScene = SplashSceneFile(fileNamed: "scenes/SplashScene.sks")!
-splashScene.scaleMode = .aspectFit
-mainView.presentScene(splashScene)
+//------------------------------------------------------------------------------------------------------------------------
+//let mainView = SKView(frame: mainFrame)
+//mainView.showsFPS = true
+//mainView.showsNodeCount = true
+//mainView.showsPhysics = false
+//let splashScene: SKScene = SplashSceneFile(fileNamed: "scenes/SplashScene.sks")!
+//splashScene.scaleMode = .aspectFit
+//mainView.presentScene(splashScene)
+//PlaygroundPage.current.liveView = mainView
+//------------------------------------------------------------------------------------------------------------------------
 PlaygroundPage.current.liveView = mainView
 //------------------------------------------------------------------------------------------------------------------------
