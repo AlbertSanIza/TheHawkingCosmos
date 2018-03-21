c4071274e690e03f6d49101e380dbcfb16c3e869
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-20 21:20:49 -0700

    changged size

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -6,8 +6,8 @@ import Foundation
 import PlaygroundSupport
 //------------------------------------------------------------------------------------------------------------------------
 //let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
-let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
-//let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
+//let mainFrame = CGRect(x: 0, y: 0, width: 800, height: 600)
+let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 //------------------------------------------------------------------------------------------------------------------------
 class ViewController: NSViewController {
     var scnView: SCNView!
