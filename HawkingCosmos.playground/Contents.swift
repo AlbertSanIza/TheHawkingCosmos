c7349ebe163f44f1ab1724843dd6a8277990fb43
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-20 23:04:32 -0700

    add more data

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -227,10 +227,16 @@ public class sceneFiles {
 //------------------------------------------------------------------------------------------------------------------------
 public class Planets {
     public let info = [
-        ["name": "sun", "size": 800.0, "distance": CGFloat(30.0), "rotation": 0.002, "translation": 3.0],
-        ["name": "mercury", "size": 50.0, "distance": CGFloat(1200.0), "rotation": 0.005, "translation": 1.607],
-        ["name": "venus", "size": 95.0, "distance": CGFloat(1450.0), "rotation": 0.005, "translation": 1.174],
-        ["name": "earth", "size": 100.0, "distance": CGFloat(1850.0), "rotation": 0.005, "translation": 1.0]
+        ["name": "sun", "size": CGFloat(800.0), "distance": CGFloat(30.0), "rotation": 0.002, "translation": 3.0],
+        ["name": "mercury", "size": CGFloat(50.0), "distance": CGFloat(1200.0), "rotation": 0.005, "translation": 1.607],
+        ["name": "venus", "size": CGFloat(95.0), "distance": CGFloat(1450.0), "rotation": 0.005, "translation": 1.174],
+        ["name": "earth", "size": CGFloat(100.0), "distance": CGFloat(1850.0), "rotation": 0.005, "translation": 1.0],
+        ["name": "mars", "size": CGFloat(60.0), "distance": CGFloat(2200.0), "rotation": 0.005, "translation": 0.802],
+        ["name": "jupiter", "size": CGFloat(500.0), "distance": CGFloat(2850.0), "rotation": 0.005, "translation": 0.434],
+        ["name": "saturn", "size": CGFloat(400.0), "distance": CGFloat(4050.0), "rotation": 0.005, "translation": 0.323],
+        ["name": "uranus", "size": CGFloat(200.0), "distance": CGFloat(5000.0), "rotation": 0.005, "translation": 0.228],
+        ["name": "neptune", "size": CGFloat(180.0), "distance": CGFloat(5550.0), "rotation": 0.005, "translation": 0.182],
+        ["name": "pluto", "size": CGFloat(90.0), "distance": CGFloat(5900.0), "rotation": 0.005, "translation": 0.159]
     ]
 }
 //------------------------------------------------------------------------------------------------------------------------
