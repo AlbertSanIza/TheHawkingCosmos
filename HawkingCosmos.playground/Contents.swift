ef2534fc7a51164a5ff849472fe824bc96d1aca6
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-20 22:52:14 -0700

    no longer needed

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -91,17 +91,6 @@ class PlanetNode: SCNNode {
     }
 }
 //------------------------------------------------------------------------------------------------------------------------
-class RingNode: SCNNode {
-    override public init() {
-        super.init()
-        self.geometry = SCNTorus(ringRadius: 100, pipeRadius: 0.05)
-        self.geometry?.firstMaterial?.diffuse.contents = NSColor.white
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-//------------------------------------------------------------------------------------------------------------------------
 public class SplashSceneFile: SKScene {
     override public func sceneDidLoad() {
         super.sceneDidLoad()
