1a0217455bff64e05d8c16634b6ded213c8c347f
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-21 23:44:40 -0700

    disable control

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -33,7 +33,7 @@ class ViewController: NSViewController {
         scnView = self.view as! SCNView
         scnView.showsStatistics = true
         scnView.backgroundColor = NSColor.black
-        scnView.allowsCameraControl = true
+        scnView.allowsCameraControl = false
         scnView.delegate = self
         scnView.isPlaying = true
         scnScene = SCNScene()
