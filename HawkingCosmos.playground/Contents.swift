8ba4112b22d63c981600b32069a8b9b77710fcb1
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-23 23:19:47 -0700

    cahnged position

--- HawkingCosmos.playground/Contents.swift
+++ HawkingCosmos.playground/Contents.swift
@@ -53,7 +53,7 @@ class ViewController: NSViewController {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         cameraNode.camera?.zFar = 26000
-        cameraNode.position = SCNVector3(x: 0, y: 1500, z: 0)
+        cameraNode.position = SCNVector3(x: 0, y: 2500, z: 11000)
         scnScene.rootNode.addChildNode(cameraNode)
         scnScene.rootNode.addParticleSystem(SCNParticleSystem(named: "starsParticle", inDirectory: "/")!)
         starsNode = SCNNode()
