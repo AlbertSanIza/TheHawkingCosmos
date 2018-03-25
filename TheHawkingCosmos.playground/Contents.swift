25a2fae16897022591dca7a9620fb969f2b8d4e4
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-24 23:34:09 -0700

    clikc mouse logic

--- TheHawkingCosmos.playground/Contents.swift
+++ TheHawkingCosmos.playground/Contents.swift
@@ -172,6 +172,41 @@ public class MainMenuScene: SKScene {
         txtInstructions = childNode(withName: "txtInstructions") as! SKLabelNode?
         txtAbout = childNode(withName: "txtAbout") as! SKLabelNode?
     }
+    override public func keyUp(with event: NSEvent) {
+        if (mainMenuSceneStatus) {
+            goToScene(withName: String(event.keyCode))
+        }
+    }
+    override public func mouseDown(with event: NSEvent) {
+        if (mainMenuSceneStatus) {
+            let mousePoint = convertPoint(fromView: CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y))
+            if let touchedNode = nodes(at: mousePoint).first {
+                goToScene(withName: touchedNode.name!)
+            }
+        }
+    }
+    func goToScene(withName: String) {
+        switch withName {
+        case "txtBack", "53", "11":
+            finalRemoveAnd(goto: "gotoSplashScene")
+        case "txtStart", "1", "36":
+            finalRemoveAnd(goto: "gotoStartScene")
+        case "txtInstructions", "34":
+            finalRemoveAnd(goto: "gotoInstructionsScene")
+        case "txtAbout", "0":
+            finalRemoveAnd(goto: "gotoAboutScene")
+        default: break
+        }
+    }
+    func finalRemoveAnd(goto: String) {
+        mainMenuSceneStatus = false
+        txtBack.removeFromParent()
+        txtTitle.removeFromParent()
+        txtStart.removeFromParent()
+        txtInstructions.removeFromParent()
+        txtAbout.removeFromParent()
+        nc.post(name: .toViewController, object: nil, userInfo: [goto: true])
+    }
 }
 //------------------------------------------------------------------------------------------------------------------------
 public class OverlayScene: SKScene {
