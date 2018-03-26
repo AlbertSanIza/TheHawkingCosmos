a4cd3b4b6853687fb553a944d69c6bb73da42eec
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-26 07:56:11 -0700

    moved instructions Scene to its own file

--- TheHawkingCosmos.playground/Contents.swift
+++ TheHawkingCosmos.playground/Contents.swift
@@ -295,44 +295,6 @@ public class StartScene: SKScene {
     }
 }
 //------------------------------------------------------------------------------------------------------------------------
-public class InstructionsScene: SKScene {
-    let nc = NotificationCenter.default
-    var instructionsSceneStatus: Bool = true
-    var txtBack: SKLabelNode!
-    var txtTitle: SKLabelNode!
-    override public func sceneDidLoad() {
-        super.sceneDidLoad()
-        txtBack = childNode(withName: "txtBack") as! SKLabelNode?
-        txtTitle = childNode(withName: "txtTitle") as! SKLabelNode?
-    }
-    override public func keyUp(with event: NSEvent) {
-        if (instructionsSceneStatus) {
-            goToScene(withName: String(event.keyCode))
-        }
-    }
-    override public func mouseDown(with event: NSEvent) {
-        if (instructionsSceneStatus) {
-            let mousePoint = convertPoint(fromView: CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y))
-            if let touchedNode = nodes(at: mousePoint).first {
-                goToScene(withName: touchedNode.name!)
-            }
-        }
-    }
-    func goToScene(withName: String) {
-        switch withName {
-        case "txtBack", "53", "11":
-            finalRemoveAnd(goto: "gotoMainMenuScene")
-        default: break
-        }
-    }
-    func finalRemoveAnd(goto: String) {
-        instructionsSceneStatus = false
-        txtBack.removeFromParent()
-        txtTitle.removeFromParent()
-        nc.post(name: .toViewController, object: nil, userInfo: [goto: true])
-    }
-}
-//------------------------------------------------------------------------------------------------------------------------
 public class OverlayScene: SKScene {
     let nc = NotificationCenter.default
     var txtTitle: SKLabelNode!
