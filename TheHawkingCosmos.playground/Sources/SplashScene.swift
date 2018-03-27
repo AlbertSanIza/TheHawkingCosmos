3b31bb33a97d10966a4465aa3cefd5dcbf8f298d
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-26 20:39:57 -0700

    use new allNode

--- TheHawkingCosmos.playground/Sources/SplashScene.swift
+++ TheHawkingCosmos.playground/Sources/SplashScene.swift
@@ -5,21 +5,15 @@ import Foundation
 public class SplashScene: SKScene {
     let nc = NotificationCenter.default
     var splashSceneStatus: Bool = true
-    var txtTitle: SKLabelNode!
-    var txtSubTitle: SKLabelNode!
-    var txtStart: SKLabelNode!
+    var allNode: SKNode!
     override public func sceneDidLoad() {
         super.sceneDidLoad()
-        txtTitle = childNode(withName: "txtTitle") as! SKLabelNode?
-        txtSubTitle = childNode(withName: "txtSubTitle") as! SKLabelNode?
-        txtStart = childNode(withName: "txtStart") as! SKLabelNode?
+        allNode = childNode(withName: "allNode") as SKNode?
     }
     public override func keyUp(with event: NSEvent) {
         if (splashSceneStatus) {
             splashSceneStatus = false
-            txtTitle.removeFromParent()
-            txtSubTitle.removeFromParent()
-            txtStart.removeFromParent()
+            allNode.removeFromParent()
             nc.post(name: .toViewController, object: nil, userInfo: ["gotoMainMenuScene":  true])
         }
     }
