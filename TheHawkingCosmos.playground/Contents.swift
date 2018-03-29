244121323b3942091a0d2e5d4ac052980611b8b8
Albert Sanchez <albert.san.iza@gmail.com>
2018-03-28 18:19:52 -0700

    changed size

--- TheHawkingCosmos.playground/Contents.swift
+++ TheHawkingCosmos.playground/Contents.swift
@@ -10,7 +10,7 @@ import Foundation
 import PlaygroundSupport
 //------------------------------------------------------------------------------------------------------------------------
 let viewController = ViewController()
-viewController.view = SCNView(frame: ViewSize().option(Number: 1))
+viewController.view = SCNView(frame: ViewSize().option(Number: 3)) // <----- Change this number from: 1 - 4 to change size
 viewController.viewDidLoad()
 PlaygroundPage.current.liveView = viewController.view
 //------------------------------------------------------------------------------------------------------------------------
