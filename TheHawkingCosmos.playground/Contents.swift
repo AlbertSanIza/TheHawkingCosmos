d0afc84631d56cbc66b378973c4488cf09eaaa67
Albert Sanchez <albert.san.iza@gmail.com>
2018-04-01 11:30:21 -0700

    init class before

--- TheHawkingCosmos.playground/Contents.swift
+++ TheHawkingCosmos.playground/Contents.swift
@@ -5,7 +5,7 @@
 //    This is my WWDC2018 Entry...
 //
 //    There is just something you need to take in mind, the best size is #3.. if
-//    its to small or too big you can change it below on line 36... sorry for the
+//    its to small or too big you can change it below on line 37... sorry for the
 //    inconvinience ❤️
 //
 //    Thanks,
@@ -32,8 +32,9 @@ import SceneKit
 import Foundation
 import PlaygroundSupport
 //------------------------------------------------------------------------------------------------------------------------
+let viewSize = ViewSize()
 let viewController = ViewController()
-viewController.view = SCNView(frame: ViewSize().option(Number: 3)) // <----- Change this number from: 1 - 4 to change size
+viewController.view = SCNView(frame: viewSize.option(Number: 3)) // <----- Change this number from: 1 - 4 to change size
 viewController.viewDidLoad()
 PlaygroundPage.current.liveView = viewController.view
 //------------------------------------------------------------------------------------------------------------------------
